5296706971b45d5ae709406d5a766524f4099c34
HanBitPix <hanjt517@gmail.com>
2018-01-17 17:55:26 -0500

    added aria roles into html

--- js/index.js
+++ js/index.js
@@ -1,47 +1,53 @@
-
-// When start button is clicked, hides the header and will display the first question
 'use strict';
 
+// Holds the users total correct answers
 let correctAnswerCounter = 0;
+// Holds the users total incorrect answers
 let incorrectAnswerCounter = 0;
+// Inputs the users answers into an array
 let userAnswers = [];
+// Shows the current question #
 let questionCounter = 1;
 
-
-// When user clicks 'START', hides the header section and unhides the main section
+// When start button is clicked, hides the header and will display the first question
 $('.start-button').on('click', function(){
+
   // Hides the Header Section
   $('header').hide();
-  // Shows the first question
-  $('main').fadeIn(2000).show(questions(1));
-});
 
+  // Shows the first question
+  $('main').fadeIn(1000).show(questions(1));
+});
 
 // Question Counter
 $('.question-counter').text(questionCounter + ' of 10');
 
-
 //1 When submitting checks if the answer is selected
 $( "#target" ).submit(function(event) {
+  
   event.preventDefault();
 
+  // Holds the users selected answer
   let userCurrentAnswer = $(".custom-select option:selected").val();
   
-  //1.A If answer is not selected will pop-up an alert
+  //1.A If answer is not selected, will pop-up an alert
   if (userCurrentAnswer == 0){
+
     // Modal when user does not select an answer
     $('.modal-body').html(`<h3 class="modal-title" id="exampleModalLongTitle">No Escaping! Select an Answer!</h3>
-    <img class="img-fluid" src="img/riddler.png" alt="Riddler shows up"/>`);
+    <img role="img" class="img-fluid" src="img/riddler.png" alt="Riddler shows up"/>`);
   }
  
   //1.B If answer is selected will compare and show if its the correct answer
   else{
 
-    //1.A.A If correct answer, Batman will show up.
+    //1.B.A If correct answer, Batman will show up.
     if (userCurrentAnswer === answerKey(questionCounter-1)){
+
       // Modal for correct answer
       $('.modal-body').html(`<h3 class="modal-title correct-answer" id="exampleModalLongTitle">Great Job! Justice Shall Prevail!</h3>
-      <img class="img-fluid" src="img/batman.png" alt="Batman shows up"/>`);
+      <img role="img" class="img-fluid" src="img/batman.png" alt="Batman shows up"/>`);
+
       // Correct Answer Counter
       correctAnswerCounter++;
       $('.correctAnswerCounter').text(correctAnswerCounter);
@@ -50,17 +56,17 @@ $( "#target" ).submit(function(event) {
       userAnswers.push(userCurrentAnswer);
 
       // Creates a results chart of Question #, User Answers & Answer Key
-      $('tbody').append(`<tr>
-      <th scope="row">${questionCounter}</th>
-      <td>${userAnswers[questionCounter-1]}</td>
-      <td>${answers[questionCounter-1]}</td>
+      $('tbody').append(`<tr role="row">
+      <th role="columnheader" scope="row">${questionCounter}</th>
+      <td role="cell">${userAnswers[questionCounter-1]}</td>
+      <td role="cell">${answers[questionCounter-1]}</td>
       </tr>`);
 
       // Adds +1 to the current question counter
       questionCounter++;
       $('.question-counter').text(questionCounter + ' of 10');
 
-      // Question Counter at footer card
+      // Question counter for bottom of the card, displays the next question #
       $('ul').find('.active').removeClass('active').addClass('disabled').next('li').removeClass('disabled').addClass('active');
 
       // Hides question after submitting an answer
@@ -70,14 +76,15 @@ $( "#target" ).submit(function(event) {
       $('main').show(questions(questionCounter));
     }
 
-    //1.A.B If Incorrect answer will show through modal, "joker will show up"
+    //1.B.B If Incorrect answer, the correct answer will show through modal, "joker will show up"
     else{
 
       // Modal: Incorrect = Joker Shows Up
       $('.modal-body').html(`<h3 class="modal-title" id="exampleModalLongTitle">WRONG ANSWER! Correct answer is: </h3>
       <p class="answerKey">${answerKey(questionCounter-1)}</p>
-      <img class="img-fluid" src="img/joker.png" alt="Joker shows up"/>
+      <img role="img" class="img-fluid" src="img/joker.png" alt="Joker shows up"/>
       <p>You must be a villian ;)</p>`);
+
       // Incorrect Answer Counter
       incorrectAnswerCounter++;
       $('.incorrectAnswerCounter').text(incorrectAnswerCounter);
@@ -86,17 +93,17 @@ $( "#target" ).submit(function(event) {
       userAnswers.push(userCurrentAnswer);
 
       // Creates a results chart of Question #, User Answers & Answer Key
-      $('tbody').append(`<tr>
-      <th scope="row">${questionCounter}</th>
-      <td>${userAnswers[questionCounter-1]}</td>
-      <td>${answers[questionCounter-1]}</td>
+      $('tbody').append(`<tr role="row">
+      <th role="columnheader" scope="row">${questionCounter}</th>
+      <td role="cell">${userAnswers[questionCounter-1]}</td>
+      <td role="cell">${answers[questionCounter-1]}</td>
       </tr>`);
 
       // Adds +1 to the current question counter
       questionCounter++;
       $('.question-counter').text(questionCounter + ' of 10');
 
-      // Question Counter at footer card
+      // Question counter for bottom of the card, displays the next question #
       $('ul').find('.active').removeClass('active').addClass('disabled').next('li').removeClass('disabled').addClass('active');
 
       // Hides question after submitting an answer
@@ -107,43 +114,59 @@ $( "#target" ).submit(function(event) {
     }
   }
   
+  // Only if the current question is greater than 10, the quiz will end!
+  // After user answers all 10 questions, the quiz will end and the results will show
   if (questionCounter > 10){
+    // Question card will fade out
     $('main').fadeOut(800);
 
-    // Final Score
-    $('.final-score').append(`<th scope="col">Total Correct: ${correctAnswerCounter} out of 10</th>
+    // Final Score is calculated
+    $('.final-score').append(`<th role="columnheader" scope="col">Total Correct: ${correctAnswerCounter} out of 10</th>
     <th scope="col">Percentage: ${(correctAnswerCounter / 10) * 100}%</th>`);
 
     // Shows the results chart
+    // Results | Shows all: User Answers, Correct Answers, & Score
     $('.results').show(3000);
   }
 });
 
+// When reset button is clicked, resets the whole quiz and empties out the saved data
 $('.reset-button').on('click',function(){
+
+  // Resets the users total correct answers
   correctAnswerCounter = 0;
+  // Resets the users total incorrect answers
   incorrectAnswerCounter = 0;
+  // Empties the users answers 
   userAnswers = [];
+  // Resets the question counter back to 1
   questionCounter = 1;
 
+  // Updates the Question, incorrect and correct counter on the card
   $('.question-counter').text(questionCounter + ' of 10');
   $('.incorrectAnswerCounter').text(incorrectAnswerCounter);
   $('.correctAnswerCounter').text(correctAnswerCounter);
   
+  // Results are hidden
   $('.results').hide();
+
+  // Reshows the header again to begin!
   $('header').show();
   
-  $('.table').html(`<thead>
-  <tr class="final-score">
+  // Resets the results table
+  $('.table').html(`<thead role="rowgroup">
+  <tr role="row" class="final-score">
   </tr>
-  <tr>
-    <th scope="col">Question #</th>
-    <th scope="col">Your Answer</th>
-    <th scope="col">Answer Key</th>
+  <tr role="row">
+    <th role="columnheader" scope="col">Question #</th>
+    <th role="columnheader" scope="col">Your Answer</th>
+    <th role="columnheader" scope="col">Answer Key</th>
   </tr>
-</thead>
-<tbody>
-</tbody>`);
+  </thead>
+  <tbody role="rowgroup">
+  </tbody>`);
 
+  // Resets the cards bottom question counter 
   $('ul li').first().removeClass('disabled').addClass('active');
 });
 
--- js/qa.js
+++ js/qa.js
@@ -1,145 +1,143 @@
 'use strict';
 
+// This holds all the correct answers in an array from 1 - 10
+const answers = ['Bruce Wayne','Gotham City','None','Death of his parents','Thomas & Martha','Alfred Pennyworth','Dick Grayson','Talia al Ghul','Joe Chill','Detective Comics'];
+
+// Simple arrow function that checks the answer key with the question number asked
+const answerKey = (questionNumber) => answers[questionNumber];
+
 // This holds Questions # 1 - 10
 function questions(currentQuestionNumber){
   switch(currentQuestionNumber) {
   // Question #1
   case 1:
     $('#target').html(`
-      <label for="exampleFormControlSelect1">Who is Batman?</label>
-      <select name="question1" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-        <option value="0">Your Answer</option>
-        <option value="Clark Kent">Clark Kent</option>
-        <option value="Tony Stark">Tony Stark</option>
-        <option value="Bruce Wayne">Bruce Wayne</option>
-        <option value="Steve Rogers">Steve Rogers</option>
+      <label for="questionNumberOne">Who is Batman?</label>
+      <select role="listbox" name="questionNumberOne" class="custom-select custom-select-lg" id="questionNumberOne">
+        <option role="option" value="0">Your Answer</option>
+        <option role="option" value="Clark Kent">Clark Kent</option>
+        <option role="option" value="Tony Stark">Tony Stark</option>
+        <option role="option" value="Bruce Wayne">Bruce Wayne</option>
+        <option role="option" value="Steve Rogers">Steve Rogers</option>
       </select>
-      <input data-toggle="modal"  data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+      <input role="button" data-toggle="modal"  data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #2
   case 2:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">What city is Batman from?</label>
-    <select name="question2" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Metropolis">Metropolis</option>
-      <option value="Central City">Central City</option>
-      <option value="Star City">Star City</option>
-      <option value="Gotham City">Gotham City</option>
+    <label for="questionNumberTwo">What city is Batman from?</label>
+    <select role="listbox" name="questionNumberTwo" class="custom-select custom-select-lg" id="questionNumberTwo">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Metropolis">Metropolis</option>
+      <option role="option" value="Central City">Central City</option>
+      <option role="option" value="Star City">Star City</option>
+      <option role="option" value="Gotham City">Gotham City</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #3
   case 3:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">What superpower does Batman have?</label>
-    <select name="question3" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Super Strength">Super Strength</option>
-      <option value="Flight">Flight</option>
-      <option value="Speed">Speed</option>
-      <option value="None">None</option>
+    <label for="questionNumberThree">What superpower does Batman have?</label>
+    <select role="listbox" name="questionNumberThree" class="custom-select custom-select-lg" id="questionNumberThree">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Super Strength">Super Strength</option>
+      <option role="option" value="Flight">Flight</option>
+      <option role="option" value="Speed">Speed</option>
+      <option role="option" value="None">None</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #4  
   case 4:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">What was the main reason Bruce Wayne became Batman?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Death of his son">Death of his son</option>
-      <option value="To bring justice to all criminals">To bring justice to all criminals</option>
-      <option value="Death of his parents">Death of his parents</option>
-      <option value="Because of Joker">Because of Joker</option>
+    <label for="questionNumberFour">What was the main reason Bruce Wayne became Batman?</label>
+    <select role="listbox" name="questionNumberFour" class="custom-select custom-select-lg" id="questionNumberFour">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Death of his son">Death of his son</option>
+      <option role="option" value="To bring justice to all criminals">To bring justice to all criminals</option>
+      <option role="option" value="Death of his parents">Death of his parents</option>
+      <option role="option" value="Because of Joker">Because of Joker</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #5  
   case 5:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">What are the names of Bruce Wayne's parents?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Jonathan & Martha">Jonathan & Martha</option>
-      <option value="Thomas & Selina">Thomas & Selina</option>
-      <option value="Thomas & Martha">Thomas & Martha</option>
-      <option value="Jonathan & Selina">Jonathan & Selina</option>
+    <label for="questionNumberFive">What are the names of Bruce Wayne's parents?</label>
+    <select role="listbox" name="questionNumberFive" class="custom-select custom-select-lg" id="questionNumberFive">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Jonathan & Martha">Jonathan & Martha</option>
+      <option role="option" value="Thomas & Selina">Thomas & Selina</option>
+      <option role="option" value="Thomas & Martha">Thomas & Martha</option>
+      <option role="option" value="Jonathan & Selina">Jonathan & Selina</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #6  
   case 6:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">Who is Batman's guardian and favorite butler?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Alfred Pennyworth">Alfred Pennyworth</option>
-      <option value="Oswald Cobblepot">Oswald Cobblepot</option>
-      <option value="Edward Nigma">Edward Nigma</option>
-      <option value="James Gordon">James Gordon</option>
+    <label for="questionNumberSix">Who is Batman's guardian and favorite butler?</label>
+    <select role="listbox" name="questionNumberSix" class="custom-select custom-select-lg" id="questionNumberSix">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Alfred Pennyworth">Alfred Pennyworth</option>
+      <option role="option" value="Oswald Cobblepot">Oswald Cobblepot</option>
+      <option role="option" value="Edward Nigma">Edward Nigma</option>
+      <option role="option" value="James Gordon">James Gordon</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #7  
   case 7:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">Name of Batman's first Robin.</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Jason Todd">Jason Todd</option>
-      <option value="Dick Grayson">Dick Grayson</option>
-      <option value="Tim Drake">Tim Drake</option>
-      <option value="Damian Wayne">Damian Wayne</option>
+    <label for="questionNumberSeven">Name of Batman's first Robin.</label>
+    <select role="listbox" name="questionNumberSeven" class="custom-select custom-select-lg" id="questionNumberSeven">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Jason Todd">Jason Todd</option>
+      <option role="option" value="Dick Grayson">Dick Grayson</option>
+      <option role="option" value="Tim Drake">Tim Drake</option>
+      <option role="option" value="Damian Wayne">Damian Wayne</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #8  
   case 8:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">Who is the mother of Batman's son, Damian Wayne?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Catwoman">Catwoman</option>
-      <option value="Talia al Ghul">Talia al Ghul</option>
-      <option value="Wonder Woman">Wonder Woman</option>
-      <option value="Batwoman">Batwoman</option>
+    <label for="questionNumberEight">Who is the mother of Batman's son, Damian Wayne?</label>
+    <select role="listbox" name="questionNumberEight" class="custom-select custom-select-lg" id="questionNumberEight">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Catwoman">Catwoman</option>
+      <option role="option" value="Talia al Ghul">Talia al Ghul</option>
+      <option role="option" value="Wonder Woman">Wonder Woman</option>
+      <option role="option" value="Batwoman">Batwoman</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #9  
   case 9:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">Who killed Bruce Wayne's parents?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Joker">Joker</option>
-      <option value="Joe Chill">Joe Chill</option>
-      <option value="James Gordon">James Gordon</option>
-      <option value="Riddler">Riddler</option>
+    <label for="questionNumberNine">Who killed Bruce Wayne's parents?</label>
+    <select role="listbox" name="questionNumberNine" class="custom-select custom-select-lg" id="questionNumberNine">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Joker">Joker</option>
+      <option role="option" value="Joe Chill">Joe Chill</option>
+      <option role="option" value="James Gordon">James Gordon</option>
+      <option role="option" value="Riddler">Riddler</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   // Question #10  
   case 10:
     $('#target').html(`
-    <label for="exampleFormControlSelect1">What comic book title did Batman first appear?</label>
-    <select name="question4" class="custom-select custom-select-lg" id="exampleFormControlSelect1">
-      <option value="0">Your Answer</option>
-      <option value="Action Comics">Action Comics</option>
-      <option value="Detective Comics">Detective Comics</option>
-      <option value=" Superman">Superman</option>
-      <option value="Batman">Batman</option>
+    <label for="questionNumberTen">What comic book title did Batman first appear?</label>
+    <select role="listbox" name="questionNumberTen" class="custom-select custom-select-lg" id="questionNumberTen">
+      <option role="option" value="0">Your Answer</option>
+      <option role="option" value="Action Comics">Action Comics</option>
+      <option role="option" value="Detective Comics">Detective Comics</option>
+      <option role="option" value=" Superman">Superman</option>
+      <option role="option" value="Batman">Batman</option>
     </select>
-    <input data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
+    <input role="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn mt-4 btn-outline-primary btn-lg btn-block" type="submit" value="SUBMIT"/>`);
     break;
   }
-}
-
-const answers = ['Bruce Wayne','Gotham City','None','Death of his parents','Thomas & Martha','Alfred Pennyworth','Dick Grayson','Talia al Ghul','Joe Chill','Detective Comics'];
-
-// This holds all the correct answers in array
-function answerKey(questionNumber){
-  // Array of answers
-  return answers[questionNumber];
 }
